72526a52cabe7f981c2a38257485f00c8a863b42
Joshua Packer <jpacker@redhat.com>
2021-11-08 22:41:51 -0500

    Small tweaks for create hypershift cluster.
    Signed-off-by: Joshua Packer <jpacker@redhat.com>

--- hyper/create-hypershift.sh
+++ hyper/create-hypershift.sh
@@ -33,7 +33,7 @@ if [ "${curContext}" != "${HYP_MANAGEMENTCLUSTER}" ]; then
   echo ">> WARNING << * ${curContext} does not match Hypershift management cluster context ${HYP_MANAGEMENTCLUSTER}"
   echo "              * Press Ctrl-c to cancel, or press <ENTER> to switch to ${HYP_MANAGEMENTCLUSTER}"
   read
-  export KUBECONFIG=${HYP_MANAGEMENTCLUSTER}
+  oc config use-context ${HYP_MANAGEMENTCLUSTER}
 fi
 
 function restore_context() {
